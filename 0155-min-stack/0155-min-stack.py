0a11721e95d780ce57639ea01f9efa49dfd5a346
Rudra Awasthi <rudraawasthi170805@gmail.com>
2026-09-14 15:51:33 +0530

    Time: 180 ms (40.01%), Space: 24.9 MB (19.36%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.py
@@ -0,0 +1,16 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+    def push(self, val):
+        self.stack.append(val)
+        if not self.minStack or val <= self.minStack[-1]:
+            self.minStack.append(val)
+    def pop(self):
+        val = self.stack.pop()
+        if val == self.minStack[-1]:
+            self.minStack.pop()
+    def top(self):
+        return self.stack[-1]
+    def getMin(self):
+        return self.minStack[-1]
